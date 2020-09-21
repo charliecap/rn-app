d77e76df3daf49bea58de3a7856c21c96812a35a
Charlie Cline <charlesclinesdesk@gmail.com>
2020-09-21 19:53:59 -0400

    .

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to List Demo"
         onPress={() => navigation.navigate("List")}
       />
+      <Button
+        title="Go to Image Demo"
+        onPress={() => navigation.navigate("Image")}
+      />
     </View>
   );
 };
