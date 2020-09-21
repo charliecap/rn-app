8a348f72c9ceecffd853c7fc68edc7e00447afb7
Charlie Cline <charlesclinesdesk@gmail.com>
2020-09-21 19:16:59 -0400

    .

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>My First App!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>My First App!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text style={styles.text}>Go to List Demo</Text>
+      </TouchableOpacity> */}
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
